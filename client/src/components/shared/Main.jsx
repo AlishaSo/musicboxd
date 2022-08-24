1af391281cde94975b949614dc02f3494df04661
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-23 21:49:28 -0400

    Add routes

--- client/src/components/shared/Main.jsx
+++ client/src/components/shared/Main.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Albums from '../pages/Albums';
+import ErrorPage from './ErrorPage';
+
 const Main = () => {
   return (
     <main>
-      
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/albums' element={ <Albums /> } />
+        {/* <Route path='/sign-in' element={ <SignIn /> } />
+        <Route path='/sign-up' element={ <SignUp /> } />
+        <Route path='/search' element={ <Search /> } /> */}
+        <Route path='*' element={ <ErrorPage /> } />
+      </Routes>
     </main>
   )
 }
