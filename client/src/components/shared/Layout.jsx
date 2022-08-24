9c6ad8976d5b88de576e4486819599f6104291c4
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-23 21:52:55 -0400

    Display header, main and footer components

--- client/src/components/shared/Layout.jsx
+++ client/src/components/shared/Layout.jsx
@@ -1,7 +1,13 @@
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
 const Layout = () => {
   return (
     <div className='layout-div'>
-      
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
