56357567e21ad69d1409818df09c39da84430b35
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-23 21:47:17 -0400

    Add logo, navbar, and starter code for mobile menu

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,7 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const closeMenu = e => {
+      console.log(e)
+      if(e.originalTarget.tagName != 'BUTTON' && e.originalTarget.tagName != 'SPAN')
+        setMenuOpen(false);
+    }
+    document.body.addEventListener('click', closeMenu);
+
+    return () => document.body.addEventListener('click', closeMenu);
+  }, []);
+
   return (
     <header>
-      
+      <div className='wrapper'>
+        <button id='nav-toggle' className='nav-toggle' onClick={() => setMenuOpen(prevMenuOpen => !prevMenuOpen)}>
+          <span className='hamburguer'></span>
+        </button>
+        <NavLink className='logo nav-link' to='/'>🎵🎧Musicboxd</NavLink>
+        <nav className={`${menuOpen ? 'visible' : ''}`}>
+          <NavLink className='nav-link' to='/albums'>Albums</NavLink>
+          <NavLink className='nav-link' to='/sign-in'>Sign In</NavLink>
+          <NavLink className='nav-link' to='/sign-up'>Sign Up</NavLink>
+          <NavLink className='nav-link' to='/search'>🔍</NavLink>
+        </nav>
+      </div>
     </header>
   )
 }
