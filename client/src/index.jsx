618f4116ea358c2a20260c2d46d08ab057658b1a
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-28 15:31:33 -0400

    User redux provider to make use of store functi ons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <Provider store = { store }>
+      <App />
+    </Provider>
   </BrowserRouter>
   // </React.StrictMode>
 )
