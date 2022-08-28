4240527e925028362152af8e547562dcc9bfba63
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-28 15:26:09 -0400

    Add conditional rendering for login and profile button/link and modify icons

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,19 +1,36 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaRecordVinyl, FaUserAstronaut } from 'react-icons/fa'
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../../features/auth/authSlice';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const closeMenu = e => {
-      console.log(e)
-      if(e.originalTarget.tagName != 'BUTTON' && e.originalTarget.tagName != 'SPAN')
-        setMenuOpen(false);
-    }
-    document.body.addEventListener('click', closeMenu);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector(state => state.auth);
 
-    return () => document.body.addEventListener('click', closeMenu);
-  }, []);
+  // useEffect(() => {
+  //   const closeMenu = e => {
+  //     console.log(e)
+  //     if(e.originalTarget.tagName != 'BUTTON' && e.originalTarget.tagName != 'SPAN')
+  //       setMenuOpen(false);
+  //   }
+  //   document.body.addEventListener('click', closeMenu);
+
+  //   return () => document.body.addEventListener('click', closeMenu);
+  // }, []);
+
+  const profileClick = () => {
+    navigate('/dashboard');
+  }
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  }
 
   return (
     <header>
@@ -21,12 +38,21 @@ const Header = () => {
         <button id='nav-toggle' className='nav-toggle' onClick={() => setMenuOpen(prevMenuOpen => !prevMenuOpen)}>
           <span className='hamburguer'></span>
         </button>
-        <NavLink className='logo nav-link' to='/'>🎵🎧Musicboxd</NavLink>
+        <Link className='logo nav-link' to='/'>🎵🎧Musicboxd</Link>
         <nav className={`${menuOpen ? 'visible' : ''}`}>
-          <NavLink className='nav-link' to='/albums'>Albums</NavLink>
-          <NavLink className='nav-link' to='/sign-in'>Sign In</NavLink>
-          <NavLink className='nav-link' to='/sign-up'>Sign Up</NavLink>
-          <NavLink className='nav-link' to='/search'>🔍</NavLink>
+          <NavLink className='nav-link' to='/albums'><FaRecordVinyl /> Albums</NavLink>
+          { user ? 
+            (<>
+              <button className='user-btn' onClick={ profileClick }> <FaUserAstronaut /> { user.username }</button>
+              <button className='logout-btn btn' to='/login' onClick={ onLogout }><FaSignOutAlt /> Logout</button>
+              </>
+            )
+            :
+            (<>
+              <Link className='nav-link' to='/login'><FaSignInAlt /> Login</Link>
+              <Link className='nav-link' to='/sign-up'><FaUser /> Sign Up</Link></>)
+          }
+          <Link className='nav-link' to='/search'>🔍</Link>
         </nav>
       </div>
     </header>
