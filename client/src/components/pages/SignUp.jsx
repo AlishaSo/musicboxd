bd978ae8cf31c4b027cb71fdb1720dec4773c8d7
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-28 15:11:22 -0400

    Create component for user sign up

---
+++ client/src/components/pages/SignUp.jsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { signUp, reset } from '../../features/auth/authSlice';
+// import Spinner from '../shared/Spinner';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const { name, username, email, password, password2 } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message);
+    }
+
+    if(isSuccess || user) {
+      navigate('/dashboard');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const handleInput = (e) => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(password !== password2) {
+      toast.error('Passwords do not match');
+    }
+    else {
+      const userData = {
+        name, 
+        username,
+        email, 
+        password
+      }
+
+      dispatch(signUp(userData));
+    }
+  }
+
+  // if(isLoading) {
+  //   return <Spinner />;
+  // }
+
+  return (
+    <div className='sign-up-div'>
+      <h1>
+        <FaUser /> Sign Up
+        <p>Please create an account</p>
+      </h1>
+
+      <section className='form'>
+        <form onSubmit={ handleSubmit }>
+          <input 
+            type = 'text'
+            id = 'name'
+            name = 'name'
+            value = { name }
+            placeholder = 'Enter your name'
+            onChange={ handleInput }
+          />
+          <input 
+            type = 'text'
+            id = 'username'
+            name = 'username'
+            value = { username }
+            placeholder = 'Enter a username'
+            onChange={ handleInput }
+          />
+          <input 
+            type = 'email'
+            id = 'email'
+            name = 'email'
+            value = { email }
+            placeholder = 'Enter your email'
+            onChange={ handleInput }
+          />
+          <input 
+            type = 'password'
+            id = 'password'
+            name = 'password'
+            value = { password }
+            placeholder = 'Enter a password'
+            onChange={ handleInput }
+          />
+          <input 
+            type = 'password'
+            id = 'password2'
+            name = 'password2'
+            value = { password2 }
+            placeholder = 'Confirm password'
+            onChange={ handleInput }
+          />
+          <button>Submit</button>
+        </form>
+      </section>
+    </div>
+  )
+}
+
+export default SignUp;
