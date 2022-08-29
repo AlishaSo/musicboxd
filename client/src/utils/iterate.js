81a5d04fcae491a7c414178d1e5a5386c5bfa4d5
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-29 11:40:39 -0400

    Add a function to filter out and return only album ids

--- client/src/utils/iterate.js
+++ client/src/utils/iterate.js
@@ -1,11 +1,19 @@
-const iterateArr = array => {
+export const iterateToGetNames = array => {
   let string = array[0].name;
 
-  for(let i = 1; i < array.length; i++) {
+  for(let i = 1; i < array.length; i++)
     string += `, ${array[i].name}`;
-  }
 
   return string;
 }
 
-export default iterateArr;
+export const iterateToGetAlbumIds = array => {
+  return array.map(item => item.track.album.id);
+}
+
+const iterateFunctions = {
+  iterateToGetNames, 
+  iterateToGetAlbumIds
+}
+
+export default iterateFunctions;
