e30114c8805e691fd95299185a92f20c827a0899
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-30 19:39:44 -0400

    Add helper file for later when deploying to heroku

---
+++ client/src/utils/heroku-backend/apiConfig.js
@@ -0,0 +1,13 @@
+let apiUrl;
+
+const apiUrls = {
+  production: 'https://.herokuapp.com/api',
+  development: 'http://localhost:3500/api'
+}
+
+window.location.hostname === 'localhost' ?
+  apiUrl = apiUrls.development
+  :
+  apiUrl = apiUrls.production
+
+export default apiUrl;
