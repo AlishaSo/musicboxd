f93b8570c4f47addae09bb05f0721fe194e2a730
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-30 19:45:39 -0400

    Add routes for components to add and edit a review

--- client/src/components/shared/Main.jsx
+++ client/src/components/shared/Main.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Dashboard from '../pages/Dashboard';
 import Albums from '../pages/Albums';
+import AddReview from '../pages/AddReview';
+import EditReview from '../pages/EditReview';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import ErrorPage from './ErrorPage';
@@ -13,6 +15,8 @@ const Main = () => {
         <Route path='/' element={ <Home /> } />
         <Route path='/dashboard' element={ <Dashboard /> } />
         <Route path='/albums' element={ <Albums /> } />
+        <Route path='/albums/:id' element={ <AddReview /> } />
+        <Route path='/reviews/:id/edit' element={ <EditReview /> } />
         <Route path='/login' element={ <Login /> } />
         <Route path='/sign-up' element={ <SignUp /> } />
         <Route path='*' element={ <ErrorPage /> } />
