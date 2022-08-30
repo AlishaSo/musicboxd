55a0d006c2aad7ec58d1995cf8ebcacaab7e621d
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-30 19:43:06 -0400

    Create a component to display reviews and make use of it in the dashboard component

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,21 +1,48 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import Review from './Review';
+import { getReviews, reset } from '../../features/reviews/reviewSlice';
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { user } = useSelector(state => state.auth);
+  const { reviews, isLoading, isError, message } = useSelector(state => state.reviews);
 
   useEffect(() => {
+    if(isError)
+      console.log(message);
+
     if(!user) {
       navigate('/login');
     }
-  }, [user, navigate]);
+
+    dispatch(getReviews());
+
+    return () => dispatch(reset());
+  }, [user, navigate, isError, message, dispatch]);
   
   return (
     <div className='dashboard-div'>
-      <h1>Welcome, { user && user.username } </h1>
-      <p>Dashboard</p>
+      <div className='heading'>
+        <h1>Welcome, { user && user.username } </h1>
+        <p>Dashboard</p>
+      </div>
+
+      <div className='reviews-container'>
+        {
+          reviews.length > 0 ? (
+              <div className='reviews'>
+                {
+                  reviews.map(review => <Review key={ review._id } reviewData={ review } />)
+                }
+              </div>
+          )
+          :
+          <h3>No reviews yet.</h3>
+        }
+      </div>
     </div>
   )
 }
---
+++ client/src/components/pages/Review.jsx
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteReview } from '../../features/reviews/reviewSlice';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Review = props => {
+  const { 
+    _id,
+    album,
+    review
+  } = props.reviewData;
+  const { user } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    dispatch(deleteReview(_id, user.token))
+    navigate(0);
+  }
+
+  return (
+      <div>
+        <img src={ album.albumCover } alt={`Album art for ${album.artist}'s "${album.title}" album`} />
+        <h2>{ album.title }</h2>
+        <p>{ review }</p>
+        <Link to={ `/reviews/${_id}/edit` } state={{ data: props.reviewData  }}>
+          <button>Edit</button>
+        </Link>
+        <button onClick={ handleDelete }>Delete</button>
+      </div>
+  )
+}
+
+export default Review;
