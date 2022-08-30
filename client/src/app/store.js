906c3e2cf33f71efa3d1207caccb1014da9c42be
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-30 19:51:16 -0400

    Add redux reducer for reviews

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import reviewReducer from '../features/reviews/reviewSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    reviews: reviewReducer 
   }
 });
