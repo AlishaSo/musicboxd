92564472f55873db93dbc92bfd37123b10f3be44
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-30 19:53:30 -0400

    Finalize albums rendering and delete unneeded code

--- client/src/components/pages/Albums.jsx
+++ client/src/components/pages/Albums.jsx
@@ -1,41 +1,35 @@
 import { useState, useEffect } from 'react';
-import { getData } from '../../services/api';
-import { nanoid } from 'nanoid';
+import { getAlbumsFromDB } from '../../services/musicboxdApi';
+import { Link } from 'react-router-dom';
 
 const Albums = () => {
   const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      let albums = await getData();
-      console.log(albums);
-      setAlbums(albums);
+      let albumsData = await getAlbumsFromDB();
+      setAlbums(albumsData);
     }
-    // fetchData();
+    fetchData();
   }, []);
 
   const displayAlbums = () => {
     return albums.map(album => {
-      const nano = nanoid();
       return (
-      <a 
-        key = { nano } 
-        className = 'album-link'
-        href = { `${album.external_urls.spotify}` }
-      >
-        <img 
-          className = 'album-art' 
-          style = { { width:'150px', height:'150px' } } 
-          src = { `${album.images[1].url}` } 
-          alt = ''
-        />
-      </a>);
+        <Link key={ album._id } to={ `/albums/${album._id}` }>
+          <img 
+            key={ `img-${album._id}` } 
+            className = 'album-art' 
+            src = { `${album.albumCover}` } 
+            alt = ''
+          />
+        </Link>);
      })
   }
 
   return (
     <div className='albums-div'>
-      { displayAlbums }
+      { displayAlbums() }
     </div>
   )
 }
