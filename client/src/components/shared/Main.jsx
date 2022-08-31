9975d468baa86dbcf85e8b9a7829b6e7b8e2182f
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-31 00:07:16 -0400

    Set this component as the provider for the reviews and albums data provider; add routes for AlbumInfo.jsx and Album.jsx

--- client/src/components/shared/Main.jsx
+++ client/src/components/shared/Main.jsx
@@ -2,25 +2,52 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Dashboard from '../pages/Dashboard';
 import Albums from '../pages/Albums';
+import Album from '../pages/Album';
 import AddReview from '../pages/AddReview';
 import EditReview from '../pages/EditReview';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import ErrorPage from './ErrorPage';
+import { getAlbumsFromDB, getReviews } from '../../services/musicboxdApi';
+import { reviewsContext } from '../../utils/reviewContext';
+import { useEffect, useState } from 'react';
+import AlbumInfo from '../pages/AlbumInfo';
+import { albumsDataContext } from '../../utils/albumsContext';
 
 const Main = () => {
+  const [allReviews, setAllReviews] = useState([]);
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      if(localStorage.getItem('user')) {
+        let info = await getReviews();
+        setAllReviews(info);
+      }
+      let albumsData = await getAlbumsFromDB();
+      setAlbums(albumsData);
+    }
+    getData();
+  }, []);
+
   return (
     <main>
-      <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='/dashboard' element={ <Dashboard /> } />
-        <Route path='/albums' element={ <Albums /> } />
-        <Route path='/albums/:id' element={ <AddReview /> } />
-        <Route path='/reviews/:id/edit' element={ <EditReview /> } />
-        <Route path='/login' element={ <Login /> } />
-        <Route path='/sign-up' element={ <SignUp /> } />
-        <Route path='*' element={ <ErrorPage /> } />
-      </Routes>
+      <reviewsContext.Provider value={{ allReviews }}>
+        <albumsDataContext.Provider value={{ albums }}>
+          <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/dashboard' element={ <Dashboard /> } />
+            <Route path='/albums' element={ <Albums /> } />
+            <Route path='/albums/:id' element={ <Album /> } />
+            <Route path='/reviews/:id' element={ <AlbumInfo /> } />
+            <Route path='/albums/:id/add' element={ <AddReview /> } />
+            <Route path='/reviews/:id/edit' element={ <EditReview /> } />
+            <Route path='/login' element={ <Login /> } />
+            <Route path='/sign-up' element={ <SignUp /> } />
+            <Route path='*' element={ <ErrorPage /> } />
+          </Routes>
+        </albumsDataContext.Provider>
+      </reviewsContext.Provider>
     </main>
   )
 }
