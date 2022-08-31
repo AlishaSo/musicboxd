ac77c1fe7310dd4b8ef6c4e59eb8f34bb62cf282
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-31 04:52:06 -0400

    Heroku deploy take two

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
 const routes = require('./routes');
-const dotenv = require('dotenv');
 const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
@@ -13,6 +12,19 @@ app.use(cors());
 app.use(logger('dev'));
 
 app.use('/api', routes);
+
+// //Serve frontend
+// if(process.env.NODE_ENV == 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//   app.get('*', (req, res) => 
+//   res.sendFile(
+//     path.resolve(__dirname, '../', 'client', 'dist', 'index.html')
+//   ));
+// }
+// else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// }
 
 app.use(errorHandler);
 
