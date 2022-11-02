060963d003835578d40393cc341513042369ae5d
Alisha Soriano <sorianoalisha@gmail.com>
2022-11-02 15:22:28 -0400

    Replace .css styles file with .scss file; start with boilerplate partial code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css';
+import '../scss/style.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
